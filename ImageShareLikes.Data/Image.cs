b83edbfc6ab5483e82107beab6df4d90a2b7446f
Avrumi Friedman <a.friedman07@gmail.com>
2020-04-29 22:02:05 -0400

    Initial commit

---
+++ ImageShareLikes.Data/Image.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ImageShareLikes.Data
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        public DateTime DateUploaded { get; set; }
+        public int Likes { get; set; }
+
+        public bool? IsHighRes { get; set; }
+    }
+}
